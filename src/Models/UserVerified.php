c0449dbfe7279a3d308996eadbd1175a0833c708
大卫 <28youth@users.noreply.github.com>
2017-05-26 14:56:03 +0800

    认证用户模型关联user模型

--- src/Models/UserVerified.php
+++ src/Models/UserVerified.php
@@ -2,10 +2,17 @@
 
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentPc\Models;
 
+use Zhiyi\Plus\Models\User;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class UserVerified extends Model
 {
     protected $table = 'user_verified';
+
+    public function user()
+    {
+    	return $this->hasOne(User::class, 'id');
+    }
+
 }
