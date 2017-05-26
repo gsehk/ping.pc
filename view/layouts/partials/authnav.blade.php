f2d16f52c3923c05b78085af63a3f36a9de78222
HHHCODE <missu082500@163.com>
2017-05-26 11:35:31 +0800

    导航判断修改

--- view/layouts/partials/authnav.blade.php
+++ view/layouts/partials/authnav.blade.php
@@ -8,7 +8,11 @@
                     <span class="nav_beta fs-16">beta</span>
                 </div>
                 <div class="login_top">
+                    @if (route::currentrouteName() == 'pc:index')
                     <a href="{{ route('pc:register') }}" class="fs-16 ">注册</a>
+                    @else
+                    <a href="{{ route('pc:index') }}" class="fs-16 ">登录</a>
+                    @endif
                     <a href="{{ route('pc:feed') }}" class="fs-16 ">随便看看</a>
                     <a href="http://www.thinksns.com" class="fs-16 ">TS+官网</a>
                 </div>
