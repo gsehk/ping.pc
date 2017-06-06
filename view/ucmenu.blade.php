1d979ca259fb1b369933e3e664ce55f91e4469e7
大卫 <28youth@users.noreply.github.com>
2017-06-06 14:34:38 +0800

    替换icon

--- view/ucmenu.blade.php
+++ view/ucmenu.blade.php
@@ -3,43 +3,43 @@
     <ul>
         <li>
             <a href="{{ route('pc:feed') }}" class="fs-16 @if ($route == 'pc:feed') dy_59 @endif">
-                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-bofang1-copy"></use></svg>全部动态
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-dynamic"></use></svg>全部动态
             </a>
         </li>
         <li>
             <a href="{{ route('pc:myFeed') }}" class="fs-16 @if ($route == 'pc:myFeed') dy_59 @endif">
-                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-bofang1-copy-copy-copy-copy-copy"></use></svg>我的动态
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-mydynamic"></use></svg>我的动态
             </a>
         </li>
         <!-- <li>
             <a href="{{ route('pc:related')}}" class="fs-16 @if ($route == 'pc:related') dy_59 @endif">
-                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-bofang1-copy-copy-copy-copy-copy-copy"></use></svg>与我相关
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-related"></use></svg>与我相关
             </a>
             <span class="dy_num">33</span>
         </li> -->
         <li>
             <a href="{{ route('pc:users') }}" class="fs-16 @if ($route == 'pc:users') dy_59 @endif">
-                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-ico_guanzhu"></use></svg>我的粉丝
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-myfans"></use></svg>我的粉丝
             </a>
         </li>
         <li>
             <a href="{{ route('pc:users', ['type'=>2]) }}" class="fs-16 @if ($route == 'pc:users') dy_59 @endif">
-                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-bofang1-copy-copy-copy-copy-copy-copy-copy"></use></svg>关注的人
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-attention"></use></svg>关注的人
             </a>
         </li>
         <li>
             <a href="{{ route('pc:rank')}}" class="fs-16 @if ($route == 'pc:rank') dy_59 @endif">
-                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-bofang1-copy-copy"></use></svg>排行榜
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-rank"></use></svg>排行榜
             </a>
         </li>
         <li>
             <a href="{{ route('pc:collection') }}" class="fs-16 @if ($route == 'pc:collection') dy_59 @endif">
-                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-bofang1-copy-copy-copy-copy"></use></svg>收藏的
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-collection"></use></svg>收藏的
             </a>
         </li>
         <li>
             <a href="{{ route('pc:account') }}" class="fs-16 @if ($route == 'pc:account') dy_59 @endif">
-                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-bofang1-copy-copy-copy"></use></svg>设置
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-setting"></use></svg>设置
             </a>
         </li>
 
