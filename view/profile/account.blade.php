7984b74d3e43e03a4a5f3e721b0c92912b884244
大卫 <28youth@users.noreply.github.com>
2017-06-08 10:32:48 +0800

    完善头像裁剪

--- view/profile/account.blade.php
+++ view/profile/account.blade.php
@@ -2,8 +2,7 @@
 
 @section('bgcolor')style="background-color:#fff"@endsection
 @section('content')
-
-    
+<link rel="stylesheet" type="text/css" href="{{ $routes['resource'] }}/cropper/cropper.min.css">
 <div class="bas_cont">
     <div class="bas_left">
         <a href="{{ route('pc:account') }}">
@@ -32,18 +31,10 @@
             <img id="J-image-preview" src="{{ $info['avatar'] }}" />
             <span class="con_cover ai_face_box">
                 <div class="ai_upload">
-                    <input id="J-file-upload"
-                           type="file"
-                           name="poster-input"
-                           data-input="#task_id"
-                           data-form="#release_form"
-                           data-preview="#J-image-preview"
-                           data-tips="#J-show-tips"
-                           data-token="{{ csrf_token()  }}"
-                    >
+                    
                     <input name="storage_task_id" id="task_id" type="hidden" value="" />
                 </div>
-                <span class="ai_btn">更换头像</span>
+                <span class="ai_btn" id="J-file-upload-btn">更换头像</span>
             </span>
         </div>
         <div class="f_div">
@@ -102,23 +93,152 @@
 @endsection
 
 @section('scripts')
+<script src="{{ $routes['resource'] }}/cropper/cropper.min.js"></script>
 <script src="{{ $routes['resource'] }}/js/birthday.js"></script>
 <script src="{{ $routes['resource'] }}/js/module.seting.js"></script>
 <script src="{{ $routes['resource'] }}/js/md5-min.js"></script>
 <script> 
-$('#J-image-preview').on('click',function(){
-        ly.load('cropper', '上传头像', '600px', '500px;');
+$('#J-image-preview, #J-file-upload-btn').on('click',function(){
+    var html = '<div id="model">'
+                    + '<div class="avatar-container" id="crop-avatar">'
+                    + '<div class="avatar-upload">'
+                    + '<input type="hidden" class="avatar-src" name="avatar_src">'
+                    + '<label for="avatarInput">选择上传图片</label>'
+                    + '<input type="file" class="avatar-input" id="avatarInput" name="avatar_file">'
+                    + '</div>'
+                    + '<div class="avatar-wrapper"></div>'
+                    + '<div class="save-btn"><span>上传完成记得点击保存按钮</span><button type="button" class="btn btn-primary avatar-save">完成</button></div>'
+                    + '</div></div>';
+    ly.loadHtml(html, '上传头像', '600px', '500px;');
+    $(function () {
+        'use strict';
+        var console = window.console || { log: function () {} };
+        function CropAvatar($element) {
+            this.$container = $element;
+            this.$avatarInput = this.$container.find('.avatar-input');
+            this.$avatarWrapper = this.$container.find('.avatar-wrapper');
+            this.$avatarPreview = this.$container.find('.avatar-preview');
+            this.$avatarSave = this.$container.find('.avatar-save');
+            this.init();
+        }
+        // base64 
+        function dataURLtoBlob(dataurl) {
+            var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+                bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+            while(n--){
+                u8arr[n] = bstr.charCodeAt(n);
+            }
+            return new Blob([u8arr], {type:mime});
+        }
+        // get round avater
+        function getRoundedCanvas(sourceCanvas) {
+            var canvas = document.createElement('canvas');
+            var context = canvas.getContext('2d');
+            var width = sourceCanvas.width;
+            var height = sourceCanvas.height;
+
+            canvas.width = width;
+            canvas.height = height;
+            context.beginPath();
+            context.arc(width / 2, height / 2, Math.min(width, height) / 2, 0, 2 * Math.PI);
+            context.strokeStyle = 'rgba(0,0,0,0)';
+            context.stroke();
+            context.clip();
+            context.drawImage(sourceCanvas, 0, 0, width, height);
+
+            return canvas;
+        }
+        CropAvatar.prototype = {
+            constructor: CropAvatar,
+            support: {
+                fileList: !!$('<input type="file">').prop('files'),
+                blobURLs: !!window.URL && URL.createObjectURL,
+                formData: !!window.FormData
+            },
+            init: function () {
+                this.support.datauri = this.support.fileList && this.support.blobURLs;
+                this.fileUpload = {};
+                this.addListener();
+            },
+            addListener: function () {
+                this.$avatarInput.on('change', $.proxy(this.change, this));
+                this.$avatarSave.on('click', $.proxy(this.click, this));
+            },
+            click: function () {
+                var croppedCanvas = this.$img.cropper('getCroppedCanvas');
+                var roundedCanvas = getRoundedCanvas(croppedCanvas); // 获取圆形头像
+                var blob = dataURLtoBlob(roundedCanvas.toDataURL());
+                      blob.name = this.fileUpload.origin_filename;
+                this.$avatarSave.text('上传中...');
+                fileUpload(blob, updateImg);
+            },
+            change: function () {
+                var files,file;
+                if (this.support.datauri) {
+                    files = this.$avatarInput.prop('files');
+                    if (files.length > 0) {
+                        file = files[0];
+                        this.fileUpload.mime_type = file.type;
+                        this.fileUpload.origin_filename = file.name;
+                        this.fileUpload.hash = '123456'+file.name;
+                        if (this.isImageFile(file)) {
+                            if (this.url) {
+                                URL.revokeObjectURL(this.url); // Revoke the old one
+                            }
+                            this.url = URL.createObjectURL(file);
+                            this.startCropper();
+                        }
+                    }
+                } else {
+                    file = this.$avatarInput.val();
+                    if (this.isImageFile(file)) {
+                        this.syncUpload();
+                    }
+                }
+            },
+            startCropper: function () {
+                var _this = this;
+                if (this.active) {
+                    this.$img.cropper('replace', this.url);
+                } else {
+                    this.$img = $('<img src="' + this.url + '">');
+                    this.$avatarWrapper.empty().html(this.$img);
+                    this.$img.cropper({
+                        aspectRatio: 1, //设置剪裁容器的比例
+                        viewMode: 1,
+                        preview: this.$avatarPreview.selector, //添加额外的元素（容器）的预览
+                    });
+                    this.active = true;
+                }
+            },
+            stopCropper: function () {
+                if (this.active) {
+                    this.$img.cropper('destroy');
+                    this.$img.remove();
+                    this.active = false;
+                }
+            },
+            isImageFile: function (file) {
+                if (file.type) {
+                    return /^image\/\w+$/.test(file.type);
+                } else {
+                    return /\.(jpg|jpeg|png|gif)$/.test(file);
+                }
+            }
+        };
+        return new CropAvatar($('#crop-avatar'));
+    });  
 });
 var username = "{{$info['name'] }}";
 var arrSelect = ["{{$info['province'] or 0}}", "{{$info['city'] or 0}}", "{{$info['area'] or 0}}"];
-$('#J-file-upload').on('change', function(e){
+/*$('#J-file-upload').on('change', function(e){
     var file = e.target.files[0];
-    console.log(file);
     fileUpload(file, updateImg);
-});
+});*/
 var updateImg = function(image, f, task_id){
     $('#task_id').val(task_id);
     $('#J-image-preview').attr('src', image.src);
+    layer.closeAll();
 }
 $(function () {
     $.ms_DatePicker({
