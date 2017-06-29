b80b996be0b79831c80899a22f1ad3f1cc11c2d8
大卫 <28youth@users.noreply.github.com>
2017-06-29 12:03:05 +0800

    添加认证模型筛选方法

--- src/Models/UserVerified.php
+++ src/Models/UserVerified.php
@@ -10,9 +10,25 @@ class UserVerified extends Model
 {
     protected $table = 'user_verified';
 
+    /**
+     * 关联认证用户信息
+     * 
+     * @return [type] [description]
+     */
     public function user()
     {
     	return $this->hasOne(User::class, 'id', 'user_id');
+    }
+
+    /**
+     * 筛选已认证数据
+     * 
+     * @param  Builder $query [description]
+     * @return [type]         [description]
+     */
+    public function scopeByAudit(Builder $query): Builder
+    {
+        return $query->where('verified', 1);
     }
 
 }
