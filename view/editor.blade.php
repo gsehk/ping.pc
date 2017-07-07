991dd95ef03e5ee3b55553e2ab406e5decdf5444
大卫 <28youth@users.noreply.github.com>
2017-07-07 16:46:25 +0800

    发布文章编辑器增加本地图片上传

--- view/editor.blade.php
+++ view/editor.blade.php
@@ -10,9 +10,10 @@
 <script type="text/javascript">
     $(function () {
         var editor = new wangEditor('editor');
-        // editor.config.hideLinkImg = true;
         editor.config.menuFixed = false;
-        editor.config.uploadImgServer = '/upload';
+        editor.config.customUpload = true;  // 配置自定义上传的开关
+        editor.config.customUploadInit = uploadInit;  // 配置上传事件，uploadInit方法已经在上面定义了
+        editor.config.uploadImgFileName = 'fileUpload'
         editor.config.menus = [
             // 'source',
             '|',     // '|' 是菜单组的分割线
@@ -25,47 +26,31 @@
             'bgcolor',
             'img'
         ];
-
-        editor.config.uploadImgParams = {
-            token: 'abcdef12345',   // 属性值会自动进行 encode ，此处无需 encode
-            passwd: '123'
-        }
-        editor.config.uploadImgHooks = {
-            before: function (xhr, editor, files) {
-                // 图片上传之前触发
-                // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，files 是选择的图片文件
-            },
-            success: function (xhr, editor, result) {
-                // 图片上传并返回结果，图片插入成功之后触发
-                // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，result 是服务器端返回的结果
-            },
-            fail: function (xhr, editor, result) {
-                alert();
-                // 图片上传并返回结果，但图片插入错误时触发
-                // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象，result 是服务器端返回的结果
-            },
-            error: function (xhr, editor) {
-                alert();
-                // 图片上传出错时触发
-                // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象
-            },
-            timeout: function (xhr, editor) {
-                // 图片上传超时时触发
-                // xhr 是 XMLHttpRequst 对象，editor 是编辑器对象
-            },
-
-            // 如果服务器端返回的不是 {errno:0, data: [...]} 这种格式，可使用该配置
-            customInsert: function (insertImg, result, editor) {
-                // 图片上传并返回结果，自定义插入图片的事件（而不是编辑器自动插入图片！！！）
-                // insertImg 是插入图片的函数，editor 是编辑器对象，result 是服务器端返回的结果
-
-                // 举例：假如上传图片成功后，服务器端返回的是 {url:'....'} 这种格式，即可这样插入图片：
-                var url = result.url
-                insertImg(url)
-            }
-        }
-
-
+        function uploadInit(){
+            // this 即 editor 对象
+            var editor = this;
+            // 编辑器中，触发选择图片的按钮的id
+            var btnId = editor.customUploadBtnId;
+            var i = document.createElement('input');
+                i.type = 'file';
+                i.name = 'fileUpload';
+                i.style.display = "none";
+            $('#'+btnId).on('click', function(){
+                i.click();
+                i.onchange = upload;
+            });
+        };
+        var upload = function(e){
+            var file = e.target.files[0];
+            editor.showUploadProgress(50);
+            fileUpload.init(file, insertHtml);
+        };
+        var insertHtml = function(image, f, storage_id){
+            var url = request_url.images + storage_id;
+            editor.command(null, 'insertHtml', '<img src="' + url + '" style="max-width:100%;"/>');
+            editor.showUploadProgress(100);
+            editor.hideUploadProgress();
+        };
         editor.create();
     });
 </script>
