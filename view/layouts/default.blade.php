d93a5a9a56053bd03af73224423c4e30e9334461
大卫 <28youth@users.noreply.github.com>
2017-07-12 13:43:36 +0800

    消息工具栏

--- view/layouts/default.blade.php
+++ view/layouts/default.blade.php
@@ -17,6 +17,7 @@
         var TOKEN = "{{ $TS['token'] or '' }}";
     </script>
     <link href="{{ $routes['resource'] }}/css/main.css" rel="stylesheet">
+    <link href="{{ $routes['resource'] }}/css/message.css" rel="stylesheet">
     <link href="{{ $routes['resource'] }}/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ $routes['resource'] }}/css/font/iconfont.css" rel="stylesheet">
     <script src="{{ $routes['resource'] }}/js/jquery.min.js"></script>
@@ -24,9 +25,13 @@
     <script src="{{ $routes['resource'] }}/js/jquery.lazyload.min.js"></script>
     <script src="{{ $routes['resource'] }}/js/font/iconfont.js"></script>
     <script src="{{ $routes['resource'] }}/js/common.js"></script>
+    <script src="{{ $routes['resource'] }}/js/module.message.js"></script>
 </head>
 
 <body @yield('body_class')>
+<script type="text/javascript">
+    message.init();
+</script>
     <div class="wrap">
         <!-- nav -->
         @include('pcview::layouts.partials.nav')
