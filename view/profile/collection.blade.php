075046871d8863f4bedace97db371ff7ccaf05a9
HHHCODE <missu082500@163.com>
2017-07-18 17:21:48 +0800

    收藏页右侧修改

--- view/profile/collection.blade.php
+++ view/profile/collection.blade.php
@@ -28,11 +28,14 @@
             <div class="dy_signed">
                 <div class="dyrTop">
                     <span class="dyrTop_r fs-14">
-                        已获积分
-                        <span class="totalnum">{{ $TS['credit'] or 0 }}</span>
+                        {{$TS['name']}}
+                        {{-- <span class="totalnum">{{ $TS['credit'] or 0 }}</span> --}}
                     </span>
-                    <img src="{{ $TS['avatar']}} " class="dyrTop_img" alt="{{ $TS['name'] }}"/>
+                    <a href="{{ route('pc:myFeed', ['user_id' => $TS['id']]) }}">
+                    <img src="{{ $TS['avatar'] }}" class="dyrTop_img" alt="{{ $TS['name'] }}"/>
+                    </a>
                 </div>
+                <div class="index_intro">{{$TS['intro']}}</div>
                 @if(empty($ischeck))
                     <div class="dy_qiandao" onclick="checkin();" id="checkin"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-qiandao1"></use></svg>每日签到<span>+5积分</span></div>
                 @else 
