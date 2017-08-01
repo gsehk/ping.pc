1d9212473f9d10c00d1e80e8b3c91fcba6f2ef26
HHHCODE <missu082500@163.com>
2017-08-01 09:13:05 +0800

    详情右侧

--- view/information/read.blade.php
+++ view/information/read.blade.php
@@ -105,7 +105,7 @@
             
             <div class="infR_top">
                 <div class="itop_autor autor_border">近期热点</div>
-                <ul class="infR_time" id="j-recent-hot">
+                <ul class="infR_time infR_time_1" id="j-recent-hot">
                     <li class="infR_time_3"><a href="javascript:;" cid="1" class="week a_border">一周</a></li>
                     <li class="infR_time_3"><a href="javascript:;" cid="2" class="month">月度</a></li>
                     <li class="infR_time_3"><a href="javascript:;" cid="3" class="quarter">季度</a></li>
