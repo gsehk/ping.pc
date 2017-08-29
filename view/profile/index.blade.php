732e3eaae848c167b22d7109e4f5ccfb38c5ff50
JsonLeex <JsonLeex@163.com>
2017-08-29 16:27:58 +0800

    个人主页-封面部分

--- view/profile/index.blade.php
+++ view/profile/index.blade.php
@@ -10,21 +10,54 @@
 
 @section('content')
 <div class="profile_top">
-    <img src="{{ $user->bg or $routes['resource'] . '/images/default_cover.png' }}" class="profile_top_cover" />
+
+    <!-- cover -->
+    <div class="profile_top_cover">
+        <img src="{{ $user->bg or $routes['resource'] . '/images/default_cover.png' }}"/>
+    </div>
+    <!-- /cover -->
+
     @if ($user->id == $TS['id'])
-    <input type="file" name="cover" style="display:none" id="cover">
-    <span class="change_cover">更换封面</span>
+        <input type="file" name="cover" style="display:none" id="cover">
+        <span class="change_cover">更换封面</span>
     @endif
+
     <div class="profile_top_info">
-        <div class="dynTop_user">{{ $user->name }}
-        </div>
-        <div class="profile_top_bio">{{ $user->bio or '这家伙很懒，什么都没留下'}}</div>
         <div class="profile_top_img">
             <a href="{{ route('pc:mine', ['user_id' => $user['id']]) }}">
                 <img src="{{ $user->avatar or $routes['resource'].'/images/avatar.png' }}" alt="{{ $user->name }}"/>
-            </a> 
+            </a>
+        </div>
+        <div class="profile_top_info_d">
+            <div class="profile_top_user"><a href="#">{{ $user->name }}</a><span>成都 · 高新</span></div>
+            <div class="profile_top_bio">{{ $user->bio or '这家伙很懒，什么都没留下'}}</div>
+            <div class="profile_top_tags">
+                <span>前端工程师</span>
+                <span>前端</span>
+                <span>前端架构师</span>
+            </div>
+            <div class="profile_logo_icon">
+                <span><i class="tag_icon"></i>已认证：TS团队成员</span>
+                <span><i class="tag_icon"></i>已认证：TS团队成员</span>
+            </div>
         </div>
     </div>
+
+    <!-- nav -->
+    <div class="profile_nav clearfix">
+        <ul class="profile_nav_list clearfix">
+            <li class="active"><a href="#">主页</a></li>
+            <li><a href="#">圈子</a></li>
+            <li><a href="#">问答</a></li>
+            <li><a href="#">草稿箱</a></li>
+        </ul>
+
+        <!-- btn -->
+        <a href="#" class="profile_nav_btn">投稿</a>
+        <!-- /btn -->
+
+    </div>
+    <!-- /nav -->
 </div>
 
 <div class="profile_body">
