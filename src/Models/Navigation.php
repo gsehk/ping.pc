d58fd98b95c7fef6fb618fed97cdcd7cf2ea372a
大卫 <daviezhang@vip.qq.com>
2017-09-12 10:18:24 +0800

    新增导航模型

---
+++ src/Models/Navigation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Zhiyi\Component\ZhiyiPlus\PlusComponentPc\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+class Navigation extends Model
+{
+
+    protected $table = 'navigation';
+
+}
