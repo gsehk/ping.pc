a67797cb455b9bc678c31f8d53f4de2fd16167fa
张卫 <daviezhang@vip.qq.com>
2017-09-14 17:55:57 +0800

    头部提取~。

--- view/profile/group.blade.php
+++ view/profile/group.blade.php
@@ -10,47 +10,9 @@
 @endsection
 
 @section('content')
-<div class="profile_top">
 
-    <div class="profile_top_cover">
-        <img class="user_bg" src="{{ $user->bg or asset('zhiyicx/plus-component-pc/images/default_cover.png') }}"/>
-    </div>
-
-    @if ($user->id == $TS->id)
-        <input type="file" name="cover" style="display:none" id="cover">
-        <span class="change_cover">更换封面</span>
-    @endif
-
-    <div class="profile_top_info">
-        <div class="profile_top_img">
-            <a href="{{ route('pc:mine', $user->id) }}">
-                <img src="{{ $user->avatar or asset('zhiyicx/plus-component-pc/images/avatar.png') }}"/>
-            </a>
-        </div>
-        <div class="profile_top_info_d">
-            <div class="profile_top_user">
-                <a href="{{ route('pc:mine', $user->id) }}">{{ $user->name }}</a>
-                <span>{{$user->location or '未知'}}</span>
-            </div>
-            <div class="profile_top_bio">{{ $user->bio or '这家伙很懒，什么都没留下'}}</div>
-            <div class="profile_top_tags">
-                @foreach ($user->tags as $tag)
-                    <span>{{$tag->name}}</span>
-                @endforeach
-            </div>
-            @if ($user->verified)
-                <div class="profile_logo_icon">
-                    <span><i class="tag_icon"></i>已认证：TS团队成员</span>
-                </div>
-            @endif
-
-        </div>
-    </div>
-
-    {{-- navbar个人中心导航栏 --}}
-    @include('pcview::profile.navbar')
-
-</div>
+{{-- 个人中心头部导航栏 --}}
+@include('pcview::profile.navbar')
 
 <div class="profile_body">
     <div class="collect_box">
--- view/profile/navbar.blade.php
+++ view/profile/navbar.blade.php
@@ -1,32 +1,69 @@
-{{-- 个人中心导航栏 --}}
-<div class="profile_nav clearfix">
-    @if ($TS->id == $user->id)
-        <ul class="profile_nav_list clearfix">
-            <li @if($current == 'feeds') class="active" @endif><a href="{{ route('pc:mine', $user['id']) }}">主页</a></li>
+{{-- 个人中心头部个人信息 --}}
+<div class="profile_top">
+    <div class="profile_top_cover">
+        <img class="user_bg" src="{{ $user->bg or asset('zhiyicx/plus-component-pc/images/default_cover.png') }}"/>
+    </div>
 
-            <li @if($current == 'group') class="active" @endif><a href="{{ route('pc:profilegroup') }}">圈子</a></li>
+    @if ($user->id == $TS->id)
+        <input type="file" name="cover" style="display:none" id="cover">
+        <span class="change_cover">更换封面</span>
+    @endif
 
-            <li @if($current == 'news') class="active" @endif><a href="{{ route('pc:profilenews') }}">资讯</a></li>
-
-            <li @if($current == 'collect') class="active" @endif><a href="{{ route('pc:profilecollect') }}">收藏</a></li>
-        </ul>
-
-        <a class="btn btn-primary contribute-btn" href="{{ route('pc:newsrelease') }}">
-            <svg class="icon"><use xlink:href="#icon-feiji"></use></svg>投稿
-        </a>
-    @else
-        <ul class="profile_nav_list clearfix">
-            <li @if($current == 'feeds') class="active" @endif><a href="{{ route('pc:mine', $user['id']) }}">TA的主页</a></li>
-
-            <li @if($current == 'group') class="active" @endif><a href="{{ route('pc:profilegroup', $user->id) }}">TA的圈子</a></li>
-        </ul>
-
-        <div class="follow-box">
-            @if ($user->hasFollower == 0)
-                <div id="follow" status="0" class="tcolor">+关注</div>
-            @else
-                <div id="follow" status="1" class="followed">已关注</div>
+    <div class="profile_top_info">
+        <div class="profile_top_img">
+            <a href="{{ route('pc:mine', $user->id) }}">
+                <img src="{{ $user->avatar or asset('zhiyicx/plus-component-pc/images/avatar.png') }}?s=150"/>
+            </a>
+        </div>
+        <div class="profile_top_info_d">
+            <div class="profile_top_user">
+                <a href="{{ route('pc:mine', $user->id) }}">{{ $user->name }}</a>
+                <span>{{$user->location or '未知'}}</span>
+            </div>
+            <div class="profile_top_bio">{{ $user->bio or '这家伙很懒，什么都没留下'}}</div>
+            <div class="profile_top_tags">
+                @foreach ($user->tags as $tag)
+                    <span>{{$tag->name}}</span>
+                @endforeach
+            </div>
+            @if ($user->verified)
+                <div class="profile_logo_icon">
+                    <span><i class="tag_icon"></i>已认证：TS团队成员</span>
+                </div>
             @endif
         </div>
-    @endif
+    </div>
+
+    {{-- 个人中心导航栏 --}}
+    <div class="profile_nav clearfix">
+        @if ($TS->id == $user->id)
+            <ul class="profile_nav_list clearfix">
+                <li @if($current == 'feeds') class="active" @endif><a href="{{ route('pc:mine', $user['id']) }}">主页</a></li>
+
+                <li @if($current == 'group') class="active" @endif><a href="{{ route('pc:profilegroup') }}">圈子</a></li>
+
+                <li @if($current == 'news') class="active" @endif><a href="{{ route('pc:profilenews') }}">资讯</a></li>
+
+                <li @if($current == 'collect') class="active" @endif><a href="{{ route('pc:profilecollect') }}">收藏</a></li>
+            </ul>
+
+            <a class="btn btn-primary contribute-btn" href="{{ route('pc:newsrelease') }}">
+                <svg class="icon"><use xlink:href="#icon-feiji"></use></svg>投稿
+            </a>
+        @else
+            <ul class="profile_nav_list clearfix">
+                <li @if($current == 'feeds') class="active" @endif><a href="{{ route('pc:mine', $user['id']) }}">TA的主页</a></li>
+
+                <li @if($current == 'group') class="active" @endif><a href="{{ route('pc:profilegroup', $user->id) }}">TA的圈子</a></li>
+            </ul>
+
+            <div class="follow-box">
+                @if ($user->hasFollower == 0)
+                    <div id="follow" status="0" class="tcolor">+关注</div>
+                @else
+                    <div id="follow" status="1" class="followed">已关注</div>
+                @endif
+            </div>
+        @endif
+    </div>
 </div>
