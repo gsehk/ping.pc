bc96bf1cd1daef25568713ecc24c4914f6f3f3d0
张卫 <daviezhang@vip.qq.com>
2017-10-13 17:46:32 +0800

    管理中心加认证图标

--- view/account/sidebar.blade.php
+++ view/account/sidebar.blade.php
@@ -2,22 +2,28 @@
 <div class="account_l">
     <ul class="account_menu">
         <a href="{{ Route('pc:account') }}">
-            <li class="@if ($account_cur == 'index')active @endif"><i class="iconfont icon-ziliao"></i>基本资料</li>
+            <li class="@if ($account_cur == 'index')active @endif">
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-ziliao"></use></svg>基本资料</li>
         </a>
         <a href="{{ Route('pc:tags') }}">
-            <li class="@if ($account_cur == 'tags')active @endif"><i class="iconfont icon-ziliao"></i>标签管理</li>
+            <li class="@if ($account_cur == 'tags')active @endif">
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-biaoqian"></use></svg>标签管理</li>
         </a>
         <a href="{{ Route('pc:authenticate') }}">
-            <li class="@if ($account_cur == 'authenticate')active @endif"><i class="iconfont icon-ziliao"></i>认证管理</li>
+            <li class="@if ($account_cur == 'authenticate')active @endif">
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-renzheng3-copy"></use></svg>认证管理</li>
         </a>
         <a href="{{ Route('pc:security')}}">
-            <li class="@if ($account_cur == 'security')active @endif"><i class="iconfont icon-ziliao"></i>安全设置</li>
+            <li class="@if ($account_cur == 'security')active @endif">
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-xiugaimima"></use></svg>安全设置</li>
         </a>
         <a href="{{ Route('pc:wallet')}}">
-            <li class="@if ($account_cur == 'wallet')active @endif"><i class="iconfont icon-ziliao"></i>我的钱包</li>
+            <li class="@if ($account_cur == 'wallet')active @endif">
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-qianbao"></use></svg>我的钱包</li>
         </a>
         <a href="{{ Route('pc:binds')}}">
-            <li class="@if ($account_cur == 'binds')active @endif"><i class="iconfont icon-ziliao"></i>账号管理</li>
+            <li class="@if ($account_cur == 'binds')active @endif">
+                <svg class="icon" aria-hidden="true"><use xlink:href="#icon-zhanghaoguanli-"></use></svg>账号管理</li>
         </a>
     </ul>
 </div>
