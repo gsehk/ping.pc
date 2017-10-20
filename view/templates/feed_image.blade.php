33b7dabcf35e684a7a6023ea39e7171690d7e14e
HHHCODE <missu082500@163.com>
2017-10-20 15:32:02 +0800

    付费图片

--- view/templates/feed_image.blade.php
+++ view/templates/feed_image.blade.php
@@ -7,7 +7,7 @@ $style = $lockstr = '';
 
 // 付费免费
 if (isset($image['paid']) && !$image['paid']) {
-    $class = 'locked_image';
+    $class = '';
     $lockstr =  'data-node="' . $image['paid_node'] . '" data-amount=' . $image['amount'] . ' data-file="' . $image['file'] .'"';
 } else {
     $class = 'bigcursor';
@@ -31,6 +31,5 @@ if (isset($count) && $count == 'one') {
     $style = 'width:' . $w . 'px;height:' . $h . 'px';
 }
 @endphp
-{{ $lockstr }}
 
 <img style="{{$style}}" class="lazy per_image {{ $class }}" data-original="{{ getImageUrl($image, $width, $height) }}" curloc="{{$curloc}}" {{ $lockstr }}/>
