7fb6a042d4e9694b0569e5d7e82de4fceaedb4ed
HHHCODE <missu082500@163.com>
2017-10-26 18:26:18 +0800

    个人主页动态付费

--- view/profile/index.blade.php
+++ view/profile/index.blade.php
@@ -33,6 +33,7 @@
 
 @section('scripts')
 <script src="{{ asset('zhiyicx/plus-component-pc/js/module.profile.js') }}"></script>
+<script src="{{ asset('zhiyicx/plus-component-pc/js/module.weibo.js') }}"></script>
 <script src="{{ asset('zhiyicx/plus-component-pc/picshow/picShow.js') }}"></script>
 <script>
 // 加载微博
