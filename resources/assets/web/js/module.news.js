747e2a8366dd0783737c7472d0d3747e226d9e10
张卫 <daviezhang@vip.qq.com>
2018-01-27 10:28:26 +0800

    chore: 文章投稿优化

--- resources/assets/web/js/module.news.js
+++ resources/assets/web/js/module.news.js
@@ -2,65 +2,53 @@
  * 文章投稿。
  */
 $('.subject-submit').on('click', function() {
-
     var _this = this;
     if (_this.lockStatus == 1) {
         noticebox('请勿重复提交', 0);
         return;
     }
     var args = {
-        'author': $('#subject-author').val(),
-        'title': $('#subject-title').val(),
-        'subject': $('#subject-abstract').val(),
         'content': editor.value(),
-        'image': $('#subject-image').val(),
-        'from': $('#subject-from').val(),
+        'from'   : $('#subject-from').val(),
+        'image'  : $('#subject-image').val(),
+        'title'  : $('#subject-title').val(),
+        'author' : $('#subject-author').val(),
+        'subject': $('#subject-abstract').val(),
         'cate_id': $('.zy_select').data('value'),
-        'news_id': $('#news_id').val() || 0
+        'news_id': $('#news_id').val(),
+        'tags'   : [],
     };
-    var tags = [];
-    $('#J-select-tags li').each(function(index){
-        tags.push($(this).data('id'));
+    $('.tags-box span').each(function(){
+        args.tags.push($(this).data('id'));
     });
-    args.tags = tags;
-
     if (!args.title || args.title.length > 20) {
-        noticebox('文章标题为1 ~ 20个字', 0);
+        noticebox('标题为1 ~ 20个字', 0);
         return;
     }
     if (!args.cate_id) {
-        noticebox('请选择栏目', 0);
+        noticebox('请选择文章分类', 0);
         return;
     }
     if (args.subject.length > 200) {
         noticebox('摘要内容不能超过200字', 0);
         return;
     }
-    if (!args.content) {
-        noticebox('文章内容不能为空', 0);
+    if (!args.content || args.content.length > 20000) {
+        noticebox('内容为1 ~ 2W字', 0);
         return;
     }
-    if (args.content.length > 5000) {
-        noticebox('文章内容不能超过5000字', 0);
-        return;
-    }
-    /*if (!args.subject) { // 如果没有摘要，则截取内容前200字作为摘要
-        args.subject = subString(editor.getHTML().replace(/<.*?>/ig,""), 200)
-    }*/
     if (args.tags.length < 1) {
         noticebox('请选择标签', 0);
         return;
     }
-    if (!args.image || args.image == 0) {
-        /*var reg = /\@\!\[\]\((\w+)\)/;
-        var imgs = reg.exec(args.content);
-        if (imgs != null) {
-            args.image = imgs[1];
-        }*/
+    if (!args.image) {
         noticebox('请上传封面图片', 0);
         return;
     }
-
+    if (!args.image || args.image == 0) {
+        noticebox('请上传封面图片', 0);
+        return;
+    }
     if (args.news_id > 0) {
         return news.update(args);
     }
