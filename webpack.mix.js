687655ea742c69fc9e244702a1503b781006bafe
张卫 <daviezhang@vip.qq.com>
2018-01-31 15:22:44 +0800

    chore: webpack配置js合并功能

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,3 +18,21 @@ mix.sourceMaps(! mix.inProduction());
 // More documents see: https://laravel.com/docs/master/mix
 // mix.copy('resources/assets/web', 'assets/');
 mix.js('resources/assets/admin', 'admin.js');
+
+// 合并并且压缩js插件
+mix.scripts([
+    'resources/assets/web/js/axios.min.js',
+    'resources/assets/web/js/lodash.min.js',
+    'resources/assets/web/js/jquery.lazyload.min.js',
+    'resources/assets/web/js/jquery.cookie.js',
+    'resources/assets/web/js/dexie.min.js',
+    'resources/assets/web/js/iconfont.js',
+    'resources/assets/web/js/layer.js',
+], 'resources/assets/web/js/global.min.js');
+
+// 合并并且压缩环信js
+mix.scripts([
+    'resources/assets/web/js/easemob/webim.config.js',
+    'resources/assets/web/js/easemob/strophe-1.2.8.min.js',
+    'resources/assets/web/js/easemob/websdk-1.4.13.js',
+], 'resources/assets/web/js/easemob/easemob.min.js');
